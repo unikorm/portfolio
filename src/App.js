8084e775c95c8abb206ef6e84d17820df3fa4dde
unikorm <adaled00@gmail.com>
2023-11-04 23:37:43 +0100

    zacal basic strukturov createBrowserRouter ako to bude vyzerat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+
 import './App.css';
+import router from './router';
+
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
-  return ( 
-    <p>sss</p>
+  return (
+    <React.Fragment>
+      <RouterProvider router={router} />
+    </React.Fragment>
   );
 };
 
---
+++ src/router.js
@@ -0,0 +1,11 @@
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        
+    },
+]);
+
+export default router;
