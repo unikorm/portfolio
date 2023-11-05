e3d4e65a1a4b6884633c251c8a3ce71ca88975de
unikorm <adaled00@gmail.com>
2023-11-05 22:17:03 +0100

    robim header, bude tam zmena rezimov dark/light, neviem ako to spravit ale aspon sa take nieco naucim

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 
+import styles from "../styles/header.module.css";
+import logo_64 from "../images/logo-64.svg";
+import light_theme from "../images/light-50.png";
+import dark_theme from "../images/dark-50.png";
+
 const Header = () => {
+
     return (
-        <p>this will be header</p>
+        <header>
+            <section className={styles.sectionLogo}>
+                <img src={logo_64} alt="moje logo" />
+            </section>
+            <section className={styles.sectionTheme}>
+                <img src={light_theme} alt="ikonka svetleho modu" />
+                <img src={dark_theme} alt="ikonka pre tmavy rezim" />
+            </section>
+        </header>
     );
 };
 
