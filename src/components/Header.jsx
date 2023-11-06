b94fd0f46ecdab5fecf7856cd8debc408a58d35a
unikorm <adaled00@gmail.com>
2023-11-06 13:47:06 +0100

    pridal funkciu v headeri na vratenie na main ked dam click na logo, ak na main tak smooth hore, neskor spravim s motion frame nieco zaujimave

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,26 @@ import logo_64 from "../images/logo-64.svg";
 import light_theme from "../images/light-50.png";
 import dark_theme from "../images/dark-50.png";
 
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
 const Header = () => {
+
+    const location = useLocation();
+    const scrollToTop = () => {
+        if ( location.pathname !== "/" ) {
+            window.scrollTo(0, 0);
+        } else {
+            window.scrollTo({top: 0, behavior: "smooth"})
+        };
+    };
 
     return (
         <header>
             <section className={styles.sectionLogo}>
-                <img src={logo_64} alt="moje logo" />
+                <Link to="/"
+                onClick={scrollToTop}
+                ><img src={logo_64} alt="moje logo" /></Link>
             </section>
             <section className={styles.sectionTheme}>
                 <img src={light_theme} alt="ikonka svetleho modu" />
