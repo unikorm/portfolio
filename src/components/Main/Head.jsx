c16397bbed95e316a4698cef25869330f23e7da6
unikorm <adaled00@gmail.com>
2023-11-06 19:30:09 +0100

    trochu sa posuvam asi

--- src/components/Main/Head.jsx
+++ src/components/Main/Head.jsx
@@ -12,13 +12,35 @@ const Head = () => {
     const headers = {
         Authorization: `Bearer ${token}`,
     };
+    const body = {
+        "query": `query {
+        user(login: "${ghUserName}") {
+          name
+          contributionsCollection {
+            contributionCalendar {
+              colors
+              totalContributions
+              weeks {
+                contributionDays {
+                  color
+                  contributionCount
+                  date
+                  weekday
+                }
+                firstDay
+              }
+            }
+          }
+        }
+      }`
+    };
 
     const [contributions, setContributions] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(apiUrl, { headers });
+                const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers });
                 if (!response.ok) {
                     throw new Error("Network doesn't work very well");
                 };
@@ -38,9 +60,9 @@ const Head = () => {
             <h3>Adam Lednicky</h3>
             <p>just guy who trying code...</p>
             <img src={profilePhoto} alt="my profile" />
-            {contributions !== null && (
+            {contributions !== null ? 
                 <p>{contributions} my total contributions on GitHub so far</p>
-            )}
+                : <p>my total contributions on GitHub so far</p> }
             <article>
                 <p>Hi and Welcome my fellow readers on my personal website. it's pleasure to have you here. i'm aspiring <span>frontend/web developer</span>, who wants to build something what works mainly with <span>JS</span>, <span>React</span> and <span>CSS</span>, but in my great future i want work with <span>tailwindCSS</span>, <span>NEXT.js</span> or <span>TypeScript</span> and of course, much more...</p>
             </article>
