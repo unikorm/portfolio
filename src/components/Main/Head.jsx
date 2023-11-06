8d131779d27f11199bafb51e515bd29cc0b7a5cd
unikorm <adaled00@gmail.com>
2023-11-06 19:06:37 +0100

    trying put there my total amount of contributions from github, i was thinkung it will be easier

--- src/components/Main/Head.jsx
+++ src/components/Main/Head.jsx
@@ -1,17 +1,48 @@
 
 import styles from "../../styles/head.module.css";
 import profilePhoto from "../../images/profile.webp";
+import { token } from "../../privat";
+
+import { useState, useEffect } from "react";
 
 const Head = () => {
+
+    const ghUserName = "unikorm";
+    const apiUrl = `https://api.github.com/users/${ghUserName}/repos`;
+    const headers = {
+        Authorization: `Bearer ${token}`,
+    };
+
+    const [contributions, setContributions] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(apiUrl, { headers });
+                if (!response.ok) {
+                    throw new Error("Network doesn't work very well");
+                };
+                const data = await response.json();
+                const totalContributions = data.reduce((total, repo) => total + repo.stargazers_count, 0);
+                setContributions(totalContributions);
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            };
+        };
+        fetchData();
+    }, []);
 
     return (
         <section className={styles.headSection}>
             <h3>Adam Lednicky</h3>
             <p>just guy who trying code...</p>
             <img src={profilePhoto} alt="my profile" />
-            {/* here will be total amout of my contributions from github API */}
+            {contributions !== null && (
+                <p>{contributions} my total contributions on GitHub so far</p>
+            )}
             <article>
-                <p>Hi and Welcome my fellow readers on my personal website. it's pleasure to have you here. i'm aspiring <span>frontend/web developer</span>, who wants to build something what works mainly with <span>JS</span>, <span>React</span> and <span>CSS</span>, but in the my great future i want work with <span>tailwindCSS</span>, <span>NEXT.js</span> or <span>TypeScript</span> and of course, much more...</p>
+                <p>Hi and Welcome my fellow readers on my personal website. it's pleasure to have you here. i'm aspiring <span>frontend/web developer</span>, who wants to build something what works mainly with <span>JS</span>, <span>React</span> and <span>CSS</span>, but in my great future i want work with <span>tailwindCSS</span>, <span>NEXT.js</span> or <span>TypeScript</span> and of course, much more...</p>
             </article>
         </section>
     );
---
+++ src/privat.js
@@ -0,0 +1,2 @@
+
+export const token = "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";
