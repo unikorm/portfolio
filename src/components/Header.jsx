57f3a9c4f33a218550197f1fc4e684cd27cccc5d
unikorm <adaled00@gmail.com>
2023-11-07 23:57:20 +0100

    very simple version is function, theme is changing, jupiiiii

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import styles from "../styles/header.module.css";
 import logo_64 from "../images/logo-64.svg";
 import light_theme from "../images/light-50.png";
 import dark_theme from "../images/dark-50.png";
+import { ThemeContext } from "../App";
 
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+
+    const { theme, toggleTheme } = useContext(ThemeContext);
 
     const location = useLocation();
     const scrollToTop = () => {
@@ -26,8 +29,8 @@ const Header = () => {
                 ><img src={logo_64} alt="moje logo" /></Link>
             </section>
             <section className={styles.sectionTheme}>
-                <img src={light_theme} alt="ikonka svetleho modu" />
-                <img src={dark_theme} alt="ikonka pre tmavy rezim" />
+                <img src={light_theme} onClick={toggleTheme} alt="ikonka svetleho modu" />
+                <img src={dark_theme} onClick={toggleTheme} alt="ikonka pre tmavy rezim" />
             </section>
         </header>
     );
