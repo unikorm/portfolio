dfdd0c11b67cfda48ea4e045dd5520b7e8f3213e
unikorm <adaled00@gmail.com>
2023-11-07 23:30:24 +0100

    im doin it like youtube video said so for now i understand that

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@
 import './App.css';
 import router from './router';
 
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
+export const ThemeContext = createContext(null);
+
 function App() {
+
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
+  };
+
   return (
-    <div id="dark">
-      <RouterProvider router={router} />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <RouterProvider router={router} />
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
