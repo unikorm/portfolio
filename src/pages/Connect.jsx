ba3abd16943bf3ee91480f37b8508093057775ff
unikorm <adaled00@gmail.com>
2023-11-09 02:34:30 +0100

    some details, i find tutorials on that animations so today i watch them and try to do something with it

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,9 +1,22 @@
 
+import styles from "../styles/connect.module.css";
+import profilePhoto from "../images/profile.webp";
+
+import { motion } from "framer-motion";
+
 const Connect = () => {
 
     return (
-        <section>
-            
+        <section className={styles.connectSection}>
+            <article>
+                <motion.img 
+                src={profilePhoto} 
+                alt="my profile"
+                whileHover={{ scale: .7, opacity: .7 }}
+                />
+                <h4>Adam Lednicky</h4>
+                <p>Just one Homo Sapiens from planet called Earth trying to develop something working ready to connect with other human beings and diferent speacies...</p>
+            </article>
         </section>
     );
 };
