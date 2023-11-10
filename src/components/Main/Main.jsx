841ac346ecde17114cdf87f41ce816d0cd52ab77
unikorm <adaled00@gmail.com>
2023-11-10 23:41:31 +0100

    a little tough is it, but i sumarize what i need do and now i try to do it

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,68 +2,19 @@
 import BlogSection from "./BlogSection";
 import Head from "./Head";
 
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
 const Main = () => {
 
-    const [showSecretPage, setShowSecretPage] = useState(false);
+
+        
 
 
-    // secret page logic
-    useEffect(() => {
-
-        // handle scroll position on map
-        const handleScroll = () => {
-            const documentHeight = Math.max(
-                document.body.scrollHeight,
-                document.body.offsetHeight,
-                document.documentElement.clientHeight,
-                document.documentElement.scrollHeight,
-                document.documentElement.offsetHeight
-            );
-
-            const windowHeight = window.innerHeight;
-            const scrollY = window.scrollY;
-            const bottomThreshold = documentHeight - windowHeight * .3; // i think documentHeight * .7
-
-            if (scrollY > bottomThreshold) {
-                setShowSecretPage(true);
-                console.log(showSecretPage);
-            };
-
-
-        };
-
-        // handle mouse position on map
-        const handleMouseMove = (event) => {
-            const mouseY = event.clientY;
-            const windowHeight = window.innerHeight;
-
-            if (mouseY > windowHeight * .7) {
-                window.addEventListener("scroll", handleScroll);
-                console.log("mouse event active");
-                console.log(showSecretPage);
-            } else {
-                window.removeEventListener("scroll", handleScroll);
-            };
-
-        };
-
-        window.addEventListener("mousemove", handleMouseMove);
-
-        return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
-            window.removeEventListener("scroll", handleScroll);
-        };
-
-    }, []);
 
     return (
         <React.Fragment>
             <Head />
             <BlogSection />
-            <div><Link to={ showSecretPage && "/secret" }></Link></div>
         </React.Fragment>
     );
 };
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,14 +2,21 @@
 import Header from "./Header";
 
 import { Outlet } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Root = () => {
+
+    // if mouse is in bottom 10% of viewport (e.clientY) and in the same time is site scrolled to absolut bottom
+    // then active listener to know if there user scroll more
+    // then if yes open secret page
+
+    
 
     return (
         <React.Fragment>
             <Header />
             <Outlet />
+            {/* here base on the result if secret page must be open will be rendered Outlet or Secret element */}
         </React.Fragment>
     );
 };
