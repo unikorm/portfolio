ba12ab8257cf099540f7aa176012232f6f797e16
unikorm <adaled00@gmail.com>
2023-11-10 23:56:09 +0100

    my version of cursor triggering works for now

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -10,7 +10,27 @@ const Root = () => {
     // then active listener to know if there user scroll more
     // then if yes open secret page
 
-    
+    const [showSecretPage, setShowSecretPage] = useState(false);
+
+    useEffect(() => {
+
+        // trigger mouse (cursor) position
+        const handleMousePosition = (event) => {
+            const mouseY = event.clientY;  // is related to mouse events and provides information about the vertical position of the mouse pointer within the viewport
+            const windowHeight = window.innerHeight; // this is height of user actual viewport
+
+            if (mouseY > windowHeight * .9) {
+                console.log("mouse is in bottom");
+            };
+        };
+
+        window.addEventListener("mousemove", handleMousePosition);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMousePosition);
+        };
+
+    }, []);
 
     return (
         <React.Fragment>
