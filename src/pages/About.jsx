7dcff28b71c4d548b1fa705efe7914ec50a8f85a
unikorm <adaled00@gmail.com>
2023-11-10 11:55:45 +0100

    praca na about page, content, compress photo, i need much smaller photo, este connect tam dat content a tak

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,15 +21,17 @@ const About = () => {
                 initial={{rotate: -5, x: -84, y: -5}}
                 />
             </aside>
-            <article>
+            <article className={styles.aboutContent}>
                 <h5>about</h5>
-                <p></p>
+                <p>hi everybody, i'm men who love life. enjoy him, mainly while running, hiking, reading and creating stuff with code.</p>
+                <p>and what is my strong side, i think mainly improvisation in everything, simple asimilation to actual condition and </p>
+                <p>now about coding. my first and prefered language is <span>JavaScript</span>, but in the future i want write <span>TypeScript</span>, then in <span>HTML</span> and <span>CSS</span> i have strong foundation. from more specific technology is my favorite <span>REACT.js</span>, and i want learn <span>NEXT.js</span> above it, then <span>GIT</span> and basic of <span>SQL</span> is standart. and i have on list <span>Node.JS</span>, more on <span>SQL</span> and dive deeper into frontend development universe.</p>
                 <aside>
                     <h5>connect</h5>
-                    <p></p>
+                    <p>same as connect</p>
                 </aside>
                 <h5>work</h5>
-                <p></p>
+                <p>nothing from software development world yet. passionately learning and waiting for opportunities.</p>
             </article>
         </section>
     );
