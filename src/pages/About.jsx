a64c9e4ed01a036b71ca0a5ec0416a6ddd6c8878
unikorm <adaled00@gmail.com>
2023-11-21 17:07:49 +0100

    modify about me page, put there connect me link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,10 @@
 import styles from "../styles/about.module.css";
 import PhotoOne from "../images/aboutPhoto.webp";
 import PhotoTwo from "../images/aboutPhotoTwo.webp";
+import { ReactComponent as Github } from "../images/github.svg";
+import { ReactComponent as LinkedIn } from "../images/linkedin.svg";
+import { ReactComponent as Gmail } from "../images/gmail.svg";
+import { ReactComponent as Instagram } from "../images/instagram.svg";
 
 import { motion } from "framer-motion";
 import { useEffect } from "react";
@@ -33,7 +37,24 @@ const About = () => {
                 <p>now about coding. my first and prefered language is <span>JavaScript</span>, but in the future i want write <span>TypeScript</span>, then in <span>HTML</span> and <span>CSS</span> i have strong foundation. from more specific technology is my favorite <span>REACT.js</span>, and i want learn <span>NEXT.js</span> above it, then <span>GIT</span> and basic of <span>SQL</span> is standart. and i have on list <span>Node.JS</span>, more on <span>SQL</span> and dive deeper into frontend development universe.</p>
                 <aside>
                     <h5>connect</h5>
-                    <p>same as connect</p>
+                    <ul>
+                        <motion.li 
+                        whileHover={{ opacity: 1, x: 40, scale: 1.2 }}
+                        initial={{ opacity: .85, x: 0 }}
+                        ><a href="mailto:adaled00@gmail.com" ><Gmail className={styles.socialIcon} /><p>Gmail</p></a></motion.li>
+                        <motion.li
+                        whileHover={{ opacity: 1, x: 40, scale: 1.2 }}
+                        initial={{ opacity: .85, x: 0 }}
+                        ><a href="https://github.com/unikorm" target="_blank" rel="noopener noreferrer" ><Github className={styles.socialIcon} /><p>Github</p></a></motion.li>
+                        <motion.li
+                        whileHover={{ opacity: 1, x: 40, scale: 1.2 }}
+                        initial={{ opacity: .85, x: 0 }}
+                        ><a href="https://www.linkedin.com/in/adam-lednicky-17159b243" target="_blank" rel="noopener noreferrer" ><LinkedIn className={styles.socialIcon} /><p>LinkedIn</p></a></motion.li>
+                        <motion.li
+                        whileHover={{ opacity: 1, x: 40, scale: 1.2 }}
+                        initial={{ opacity: .85, x: 0 }}
+                        ><a href="https://www.instagram.com/u.m.i.nat" target="_blank" rel="noopener noreferrer" ><Instagram className={styles.socialIcon} /><p>Instagram</p></a></motion.li>
+                    </ul>
                 </aside>
                 <h5>work</h5>
                 <p>nothing from software development world yet. passionately learning and waiting for opportunities.</p>
