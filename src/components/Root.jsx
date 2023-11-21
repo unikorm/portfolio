d72395c34949b419f118f7d87d9cfc692241c278
unikorm <adaled00@gmail.com>
2023-11-21 02:49:12 +0100

    unfortunetelly, bard doesn't help either for now, i test it and he give me shits, now i try second version, this is from bard, before that i write it without npm start from scratch how i see that in three useEffect and all separated and check to stop unneccessary state changes, but it doesn't work as i hope, so, i am at the beginning here, i must admit i know nothing, this is complicated usage of useEffect, i really don't understand how it works when i apply more of them and put there ependencies and so on.... i will study and try more

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,8 +1,8 @@
 
 import Header from "./Header";
 
-import { Outlet } from "react-router-dom";
-import React from "react";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 const Root = () => {
 
@@ -10,12 +10,58 @@ const Root = () => {
     // if mouse is in bottom cca 5% of viewport (e.clientY) and in the same time is site scrolled to absolut bottom
     // then if yes open secret page
 
+    // const navigate = useNavigate();
+    // const location = useLocation();
+    // const [cursorPosition, setCursorPosition] = useState(false);
+    // const [scrolled, setScrolled] = useState(false);
+
+    const navigate = useNavigate();
+    const [cursorPosition, setCursorPosition] = useState(0);
+
+    function checkBottomConditions(e) {
+        const cursorPosition = e.clientY;
+        const scrollPosition = window.scrollY;
+        const viewportHeight = window.innerHeight;
+        const documentHeight = document.body.scrollHeight;
+      
+        if (cursorPosition >= viewportHeight - 0.05 * viewportHeight && scrollPosition >= documentHeight - viewportHeight) {
+          return true;
+        };
+      
+        return false;
+    };
+
+    const mouseMove = (e) => {
+        setCursorPosition(e.clientY);
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousemove", mouseMove);
+      }, []);
+    
+      const handleScroll = () => {
+        if (checkBottomConditions()) {
+          navigate("/secret");
+        }
+    };
+    
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+   
+      
+
     return (
         <React.Fragment>
             <Header />
-            <Outlet />    {/*  i think here must be only outlet cause it can now navigate from secret back and don't reset history like every time i am on main, now it behave normal   */}
+            <Outlet />
         </React.Fragment>
-    );  // now i realise it would be good put there at the bottom like button to open secret, like Link to="/secret", but i try this solve, what happening
+    );
 };
 
 export default Root;
