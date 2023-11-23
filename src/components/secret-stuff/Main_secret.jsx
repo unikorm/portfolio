4699f1ae0b9359fccf58156c4c22e4edad2cfeb2
unikorm <adaled00@gmail.com>
2023-11-23 23:53:08 +0100

    changes on secret page, fun with framer motion, now i create here branch on developing a game here

--- src/components/secret-stuff/Main_secret.jsx
+++ src/components/secret-stuff/Main_secret.jsx
@@ -1,13 +1,24 @@
 
 import styles from "../../styles/styles_secret/main_secret.module.css";
 
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
 const MainSecret = () => {
 
+    const constraintsRef = useRef(null);
+
     return (
+        <div className={styles.wrapper}>
         <section className={styles.mainSectionSecret}>
-            <p>here will be my art</p>
+            <motion.aside className={styles.funContainer} ref={constraintsRef}>
+                <motion.div className={styles.funItem} drag dragConstraints={constraintsRef}>
+                <p>👽</p>
+                </motion.div>
+            </motion.aside>
             
         </section>
+        </div>
     );
 };
 
